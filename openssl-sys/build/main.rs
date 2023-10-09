28d594c7e7b684cbcc7ff4b06b6eac3edac18424
niklas <niklas@appli.se>
2023-10-09 08:01:20 +0200

    Support LibreSSL 3.8.*

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -284,6 +284,7 @@ See rust-openssl documentation for more information:
             (3, 7, _) => ('3', '7', 'x'),
             (3, 8, 0) => ('3', '8', '0'),
             (3, 8, 1) => ('3', '8', '1'),
+            (3, 8, _) => ('3', '8', 'x'),
             _ => version_error(),
         };
 
@@ -326,7 +327,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL (version 1.0.1 through 1.1.1, or 3), or LibreSSL 2.5
-through 3.8.1, but a different version of OpenSSL was found. The build is now aborting
+through 3.8.x, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
